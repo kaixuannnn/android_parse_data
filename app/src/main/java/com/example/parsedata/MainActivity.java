72afb2fb224008d23f814e4205deef21c5b41891
Kai Xuan <kaixuan@agmostudio.com>
2021-11-10 09:47:32 +0800

    json array request example

--- app/src/main/java/com/example/parsedata/MainActivity.java
+++ app/src/main/java/com/example/parsedata/MainActivity.java
@@ -7,23 +7,52 @@ import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    RequestQueue queue;
 
     String url = "https://www.google.com";
+    String apiUrl = "https://jsonplaceholder.typicode.com/todos";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RequestQueue queue = Volley.newRequestQueue(this);
+        queue = Volley.newRequestQueue(this);
 
+        getJsonArrayRequest();
+
+        getString(queue);
+    }
+
+    private void getJsonArrayRequest() {
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null,
+                response ->{
+                    for (int i = 0; i < response.length(); i++) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject(i);
+                            Log.d("JSON", "onCreate: "+ jsonObject.getInt("id"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                , error ->
+                    Log.d("JSON", "OnCreate: Failed!")
+                );
+
+        queue.add(jsonArrayRequest);
+    }
+
+    private void getString(RequestQueue queue) {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
-            //displaythe contents of our url
+            //display the contents of our url
             Log.d("Main","onCreate: "+ response.substring(0,500));
         }, error -> {
             Log.d("Main","onError: "+ "Failed to get info!");
