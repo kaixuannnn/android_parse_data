0e047f85d9695818f039f61cf160c36c2904470f
Kai Xuan <kaixuan@agmostudio.com>
2021-11-10 08:58:43 +0800

    volley fetch data example

--- app/src/main/java/com/example/parsedata/MainActivity.java
+++ app/src/main/java/com/example/parsedata/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.parsedata;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url = "https://www.google.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, response -> {
+            //displaythe contents of our url
+            Log.d("Main","onCreate: "+ response.substring(0,500));
+        }, error -> {
+            Log.d("Main","onError: "+ "Failed to get info!");
+        });
+
+        queue.add(stringRequest);
     }
 }
